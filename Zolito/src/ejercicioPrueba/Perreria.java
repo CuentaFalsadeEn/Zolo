48dbfe52edcfd290973acc8176a67dd201d66ff1
alumno <alumno@PC02>
2018-02-21 13:26:21 +0100

    Si no pongo mensaje no me deja -_-

---
+++ Zolito/src/ejercicioPrueba/Perreria.java
@@ -0,0 +1,11 @@
+package ejercicioPrueba;
+
+public class Perreria {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Yo que se xd xd");
+
+	}
+
+}
