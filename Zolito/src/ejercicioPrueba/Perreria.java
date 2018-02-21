b1b60772f4f54b870c48acf739edc8234ff55e24
alumno <alumno@PC02>
2018-02-21 13:28:05 +0100

    Modificacion de dos clases

--- Zolito/src/ejercicioPrueba/Perreria.java
+++ Zolito/src/ejercicioPrueba/Perreria.java
@@ -5,6 +5,7 @@ public class Perreria {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Yo que se xd xd");
+		System.out.println("Y este tambien lo cambio :/");
 
 	}
 
--- Zolito/src/ejercicioPrueba/PrimerEjemplo.java
+++ Zolito/src/ejercicioPrueba/PrimerEjemplo.java
@@ -5,6 +5,7 @@ public class PrimerEjemplo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Nose");
+		System.out.println("Modificar algo ya subido");
 	}
 
 }
