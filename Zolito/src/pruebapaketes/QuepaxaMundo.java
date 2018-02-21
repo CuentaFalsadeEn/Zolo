8a6167ecd8d54ff355076c3327410e4577d2c6cb
alumno <alumno@PC02>
2018-02-21 13:29:29 +0100

    Meter otro package

---
+++ Zolito/src/pruebapaketes/QuepaxaMundo.java
@@ -0,0 +1,10 @@
+package pruebapaketes;
+
+public class QuepaxaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Falta de originalidad en nombres");
+	}
+
+}
